26f02418131cc6865a6e738b7405de776cd43a5d
adikrs <adityakrsingh.ece18@itbhu.ac.in>
2019-11-03 18:38:14 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'startupFair',
+    loadChildren: () => import('./startup-fair/startup-fair.module').then(m => m.StartupFairModule)
+  },
+  { 
+    path: 'thinkTalks',
+    loadChildren: () => import('./think-talks/think-talks.module').then(m => m.ThinkTalksModule) 
+  },
+  {
+    path: 'corporateConclave',
+    loadChildren: () => import('./corporate-conclave/corporate-conclave.module').then(m => m.CorporateConclaveModule)
+  },
+  {
+    path: 'initiatives',
+    loadChildren: () => import('./initiatives/initiatives.module').then(m => m.InitiativesModule) 
+  },
+  {
+    path: 'kaleidoscope',
+    loadChildren: () => import('./kaleidoscope/kaleidoscope.module').then(m => m.KaleidoscopeModule)
+  },
+  {
+    path: 'sponsers',
+    loadChildren: () => import('./sponsers/sponsers.module').then(m => m.SponsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
