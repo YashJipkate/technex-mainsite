f7ed3ffa648b31a82b412fa375b688b92302bba8
arron-tij <ronitraj.tg@gmail.com>
2019-11-10 11:18:47 +0530

    Added events component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
-
+import { Events2Component } from './events2/events2.component';
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent
-  }
-];
+  },
+  {
+    path: 'events',
+    component: Events2Component
+  },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Events2Component } from './events2/events2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    NavbarComponent
+    NavbarComponent,
+    Events2Component
   ],
   imports: [
     BrowserModule,
