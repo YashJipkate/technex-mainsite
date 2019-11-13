ccc0092989ca14be0eef3a03d9d7248f1000f7a1
adikrs <adityakrsingh.ece18@itbhu.ac.in>
2019-11-13 22:45:30 +0530

    completed routing for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  {
+    path: 'startupFair',
+    title: 'STARTUP FAIR',
+    class: ''
+  },
+  {
+    path: '#',
+    title: 'EXHIBITIONS',
+    class: ''
+  },
+  {
+    path: 'kaleidoscope',
+    title: 'KALEIDOSCOPE',
+    class: ''
+  },
+  {
+    path: 'thinkTalks',
+    title: 'THINK TALKS',
+    class: ''
+  },
+  {
+    path: '#',
+    title: 'EVENTS',
+    class: ''
+  },
+];
+
+export const ROUTS: RouteInfo[] = [
+  {
+    path: '#',
+    title: 'WORKSHOPS',
+    class: ''
+  },
+  {
+    path: 'initiatives',
+    title: 'INITIATIVES',
+    class: ''
+  }, 
+  {
+    path: 'corporateConclave',
+    title: 'CORPORATE CONCLAVE',
+    class: ''
+  },
+];
+
+export const logo: RouteInfo[] = [
+  {
+    path: '/',
+    title: '',
+    class: ''
+  },
+];
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+  menuItems: any[];
+  menuItemms: any[];
+  menuItemmms: any[];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.menuItemms = ROUTS.filter(menuItemm => menuItemm);
+    this.menuItemmms = logo.filter(menuItemmm => menuItemmm);
 
+  }  
 }
