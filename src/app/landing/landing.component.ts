cbb653f68425b905ff3533540a5b1be47022c820
arron-tij <ronitraj.tg@gmail.com>
2019-11-15 01:29:20 +0530

    merge

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  {
+    path: 'sponsors',
+    title: 'Sponsors',
+    class: ''
+  },
+];
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
+
 export class LandingComponent implements OnInit {
+  menuItems: any[];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+      this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }  
 
 }
