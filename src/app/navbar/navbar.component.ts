4835f9e70f6d5311c0608df9276a015f10701328
adikrs <44467907+adikrs@users.noreply.github.com>
2019-11-14 17:24:20 +0530

    Completed routing of navbar and added sponsors. (#7)
    * added empty static pages
    
    * added routing
    
    * added static pages1
    
    * static pages added
    
    * overlapping text issue resolved
    
    * overlapping text issue resolved
    
    * updated text display
    
    * animation fade-in added
    
    * spaces and pages modified
    
    * fixed identation and newline
    
    * fixed identation
    
    * fixed spaces b/w colons
    
    * fixed identation
    
    * completed routing for navbar
    
    * Resolved conflicts
    
    * Resolved conflicts
    
    * Resolved conflicts
    
    * corrected names of varaiables
    
    * updated navbar logo and font
    
    * sponsors position fixed
    
    * Update src/app/navbar/navbar.component.html
    
    * Update src/app/landing/landing.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  class: string;
+}
+
+export const LEFTROUTES: RouteInfo[] = [
+  {
+    path: 'startupFair',
+    title: 'STARTUP FAIR',
+    class: ''
+  },
+  {
+    path: '#',
+    title: 'EXHIBITIONS',
+    class: ''
+  },
+  {
+    path: 'kaleidoscope',
+    title: 'KALEIDOSCOPE',
+    class: ''
+  },
+  {
+    path: 'thinkTalks',
+    title: 'THINK TALKS',
+    class: ''
+  },
+  {
+    path: '#',
+    title: 'EVENTS',
+    class: ''
+  },
+];
+
+export const RIGHTROUTES: RouteInfo[] = [
+  {
+    path: '#',
+    title: 'WORKSHOPS',
+    class: ''
+  },
+  {
+    path: 'initiatives',
+    title: 'INITIATIVES',
+    class: ''
+  }, 
+  {
+    path: 'corporateConclave',
+    title: 'CORPORATE CONCLAVE',
+    class: ''
+  },
+];
+
+export const LOGOROUTES: RouteInfo[] = [
+  {
+    path: '/',
+    title: '',
+    class: ''
+  },
+];
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
+  leftmenuItems: any[];
+  rightmenuItems: any[];
+  logomenuItems: any[];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.leftmenuItems = LEFTROUTES.filter(menuItem => menuItem);
+    this.rightmenuItems = RIGHTROUTES.filter(menuItemm => menuItemm);
+    this.logomenuItems = LOGOROUTES.filter(menuItemmm => menuItemmm);
 
+  }  
 }
