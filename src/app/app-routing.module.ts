6ba41069b5b4c73b3ba739e8c61e605a1c9790b3
arron-tij <ronitraj.tg@gmail.com>
2019-11-15 11:21:51 +0530

    Phir hera pheri+favicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,45 +11,45 @@ const routes: Routes = [
   {
     path: 'events',
     component: EventsComponent ,
-    data: {title: 'Technex | Events'} 
+    data: {title: 'Events | Technex'} 
   },
   {
     path: 'startupFair',
     loadChildren: () => import('./startup-fair/startup-fair.module').then(m => m.StartupFairModule),
-    data: {title: 'Technex | Startup Fair'} 
+    data: {title: 'Startup Fair | Technex'} 
   },
   { 
     path: 'thinkTalks',
     loadChildren: () => import('./think-talks/think-talks.module').then(m => m.ThinkTalksModule),
-    data: {title: 'Technex | Think Talks'} 
+    data: {title: 'Think Talks | Technex'} 
   },
   {
     path: 'corporateConclave',
     loadChildren: () => import('./corporate-conclave/corporate-conclave.module').then(m => m.CorporateConclaveModule),
-    data: {title: 'Technex | Corporate Conclave'} 
+    data: {title: 'Corporate Conclave | Technex'} 
   },
   {
     path: 'initiatives',
     loadChildren: () => import('./initiatives/initiatives.module').then(m => m.InitiativesModule), 
-    data: {title: 'Technex | Initiatives'} 
+    data: {title: 'Initiatives | Technex'} 
   },
   {
     path: 'kaleidoscope',
     loadChildren: () => import('./kaleidoscope/kaleidoscope.module').then(m => m.KaleidoscopeModule),
-    data: {title: 'Technex | Kaleidoscope'} 
+    data: {title: 'Kaleidoscope | Technex'} 
   },
   {
     path: 'sponsors',
     loadChildren: () => import('./sponsors/sponsors.module').then(m => m.SponsorsModule),
-    data: {title: 'Technex | Events'} 
+    data: {title: 'Sponsors | Technex'} 
   },
   { path: 'exhibitions', 
     loadChildren: () => import('./exhibitions/exhibitions.module').then(m => m.ExhibitionsModule), 
-    data: {title: 'Technex | Exhibitions'} 
+    data: {title: 'Exhibitions | Technex'} 
   },
   { path: 'workshops', 
     loadChildren: () => import('./workshops/workshops.module').then(m => m.WorkshopsModule), 
-    data: {title: 'Technex | Workshops'} 
+    data: {title: 'Workshops | Technex'} 
   }
 ];
 
