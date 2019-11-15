ba5f45f03e204f93317b9fd62a1c7c5428e4e90a
arron-tij <ronitraj.tg@gmail.com>
2019-11-15 10:30:58 +0530

    revert

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -61,68 +61,6 @@ export const LOGOROUTES: RouteInfo[] = [
   },
 ];
 
-
-declare interface RouteInfo {
-  path: string;
-  title: string;
-  class: string;
-}
-
-export const LEFTROUTES: RouteInfo[] = [
-  {
-    path: 'startupFair',
-    title: 'STARTUP FAIR',
-    class: ''
-  },
-  {
-    path: '#',
-    title: 'EXHIBITIONS',
-    class: ''
-  },
-  {
-    path: 'kaleidoscope',
-    title: 'KALEIDOSCOPE',
-    class: ''
-  },
-  {
-    path: 'thinkTalks',
-    title: 'THINK TALKS',
-    class: ''
-  },
-  {
-    path: '#',
-    title: 'EVENTS',
-    class: ''
-  },
-];
-
-export const RIGHTROUTES: RouteInfo[] = [
-  {
-    path: '#',
-    title: 'WORKSHOPS',
-    class: ''
-  },
-  {
-    path: 'initiatives',
-    title: 'INITIATIVES',
-    class: ''
-  }, 
-  {
-    path: 'corporateConclave',
-    title: 'CORPORATE CONCLAVE',
-    class: ''
-  },
-];
-
-export const LOGOROUTES: RouteInfo[] = [
-  {
-    path: '/',
-    title: '',
-    class: ''
-  },
-];
-
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
