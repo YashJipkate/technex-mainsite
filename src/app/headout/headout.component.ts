899d5a7b90e7994297b6990e059e6904b2f1c6b0
Subodh Verma <subodhlucky6262@gmail.com>
2020-01-13 23:41:32 +0530

    New landing (#71)
    * Updated landing page and various minor UI and image changes.
    
    * New landing page and various minor UI and image changes.

---
+++ src/app/headout/headout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headout',
+  templateUrl: './headout.component.html',
+  styleUrls: ['./headout.component.css']
+})
+export class HeadoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
