720eb6c0eb2c3bb89a64829b60bfb2b6db0a8874
Ronit Raj <40382117+arron-tij@users.noreply.github.com>
2020-02-01 18:35:20 +0530

    ch (#103)

---
+++ src/app/w91/w91.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-w91',
+  templateUrl: './w91.component.html',
+  styleUrls: ['./w91.component.css']
+})
+export class W91Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
